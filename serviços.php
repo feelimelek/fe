14ef98ef16c60f7f6a23a0599a16fc90ac4bf9af
feelimelek <80131744+feelimelek@users.noreply.github.com>
2021-06-25 15:29:06 -0300

    Add files via upload

---
+++ serviços.php
@@ -0,0 +1,24 @@
+<div id="main" class="center">
+	<span style="color:black"> -SERVIÇOS-</span>
+	<h2> Esses são os nossos serviços</h2>
+</div>
+<div id="serviços">
+	<div class="container">
+		<table class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Dia</th>
+					<th>Atividade</th>
+					<th>Responsáveis</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>14 de Fevereiro</td>
+					<td>Criação do site</td>
+					<td>Fernanda</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
